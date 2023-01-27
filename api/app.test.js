e2bc4daaa09e1696460353e799ee6693fcd01c9d
Demian143 <patrickxavier140@gmail.com>
2023-01-27 14:55:42 -0300

    Adding before each to setup enviroment

--- api/app.test.js
+++ api/app.test.js
@@ -2,8 +2,8 @@ import supertest from "supertest";
 import app from "./app.js";
 import fs from 'fs';
 
-async function clearDB() {
-    fs.readFile('./api/db.json', 'utf-8', (err, data) => {
+function cleanDB() {
+    return fs.readFile('./api/db.json', 'utf-8', (err, data) => {
         if (err) {
             console.log(err);
             return;
@@ -12,7 +12,7 @@ async function clearDB() {
         const json = JSON.parse(data)
         json.users = []
 
-        fs.writeFile('./api/db.json', JSON.stringify(json), (err) => {
+        fs.writeFileSync('./api/db.json', JSON.stringify(json), (err) => {
             if (err) {
                 console.log(err);
                 return;
@@ -22,35 +22,28 @@ async function clearDB() {
 }
 
 // TODO: increase the timeout value of tests
-
 beforeEach(async () => {
-    await clearDB();
+    await cleanDB();
 });
 
-afterEach(async () => {
-    await clearDB();
+
+it('GET / --> Empty array', async () => {
+    await supertest(app).get('/')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect('{"users":[]}');
 });
 
-describe('GET /', () => {
-
-    it('GET / --> Empty array', async () => {
-        await supertest(app).get('/')
-            .expect(200)
-            .expect('Content-Type', /json/)
-            .expect('{"users":[]}');
-    });
-
-    it('GET / --> Array with one user', async () => {
-        const user = {
-            name: 'Debian',
-            age: '23',
-            city: 'São Gonçalo',
-            state: 'RJ',
-            country: 'Brazil'
-        };
-        await supertest(app)
-            .post('/?name=Debian&age=23&city=São Gonçalo&state=RJ&country=Brazil')
-            .expect(user)
-            .expect(201);
-    });
+it('GET / --> Array with one user', async () => {
+    const user = {
+        name: 'Debian',
+        age: '23',
+        city: 'São Gonçalo',
+        state: 'RJ',
+        country: 'Brazil'
+    };
+    await supertest(app)
+        .post('/?name=Debian&age=23&city=São Gonçalo&state=RJ&country=Brazil')
+        .expect(user)
+        .expect(201);
 });
